994acd9d222f5d4ba1d5c5180592eea19705ca62
Orlando <59018852+ingkoon@users.noreply.github.com>
2020-09-27 00:05:33 +0900

    2-4UDP

---
+++ 2-4/UDP2_4_cl.py
@@ -0,0 +1,13 @@
+from socket import *
+
+serverName = 'hostname'
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+message = input('input loewrcase sentence: ')
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+print(modifiedMessage)
+clientSocket.close()
