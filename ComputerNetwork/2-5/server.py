a337a1e54f7ff75768df3b17b142b3008422deef
Orlando <59018852+ingkoon@users.noreply.github.com>
2020-09-27 13:48:00 +0900

    grouping
    grouping this code

--- ComputerNetwork/2-5/server.py
+++ ComputerNetwork/2-5/server.py
@@ -2,10 +2,10 @@ import socket
 
 server_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_socket.bind(('localhost',2222))
+server_socket.bind(('localhost',12345))
 server_socket.listen(0)
 
 client_socket, addr = server_socket.accept()
-data = client_socket.recv(62235)
+data = client_socket.recv(65535)
 
 print(data.decode())
