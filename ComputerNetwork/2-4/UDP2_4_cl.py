5b58b4a6ec9e2ecb8f87aaeb6d9b6f27cf6547ce
Orlando <59018852+ingkoon@users.noreply.github.com>
2020-09-28 20:07:29 +0900

    client final

--- ComputerNetwork/2-4/UDP2_4_cl.py
+++ ComputerNetwork/2-4/UDP2_4_cl.py
@@ -1,9 +1,12 @@
 #컴네 2-4
+#20151167 이인재
 from socket import *
 
+#서버이름과 포트번호 설정
 serverName = 'localhost'
 serverPort = 12000
 
+#UDP소켓 생성
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 #소문자 메세지를 입력받도록 안내
@@ -14,6 +17,9 @@ clientSocket.sendto(message.encode(),(serverName, serverPort))
 
 #서버가 보낸 메세지와 서버주소를 받아들이기 위한 변수
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+#서버로 부터 전송받은 메세지 출력
 print(modifiedMessage.decode())
 
+#클라이언트 소켓 종료
 clientSocket.close()
