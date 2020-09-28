e73996905e85122af33e01011ac75567e331920c
Orlando <59018852+ingkoon@users.noreply.github.com>
2020-09-28 19:34:13 +0900

    cn final

--- ComputerNetwork/2-4/UDP2_4_cl.py
+++ ComputerNetwork/2-4/UDP2_4_cl.py
@@ -1,14 +1,19 @@
 #컴네 2-4
 from socket import *
 
-serverName = 'hostname'
+serverName = 'localhost'
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+#소문자 메세지를 입력받도록 안내
 message = input('input loewrcase sentence: ')
-clientSocket.sendto(message,(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 
-print(modifiedMessage)
+#클라이언트 소켓을 통해 메세지를 서버에 전송
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+
+#서버가 보낸 메세지와 서버주소를 받아들이기 위한 변수
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+
 clientSocket.close()
--- ComputerNetwork/2-4/UDP2_4_se.py
+++ ComputerNetwork/2-4/UDP2_4_se.py
@@ -1,14 +1,17 @@
 #컴네 2-4
+#서버 소스코드
 from socket import *
 
 serverPort = 12000
-
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+serverSocket.bind(('localhost', serverPort))
 
 print("The server is ready to receive")
 
 while 1:
+    #소켓을 통해 클라이언트가 보내는 메시지와 클라이이너트의 주소를 갖게 된다
     message, clientAddress = serverSocket.recvfrom(2048)
+    #클라이언트가 보낸 메세지를 대문자로 바꿔준다.
     modifiedMessage = message.upper()
+    #소켓을 통해 다시 변환시킨 문자들을 클라이언트의 주소로 전송
     serverSocket.sendto(modifiedMessage, clientAddress)
