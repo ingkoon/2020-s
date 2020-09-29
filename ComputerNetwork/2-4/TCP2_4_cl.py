f82e6fd0dd10cf6707a296731aab70a5af6d581d
Orlando <59018852+ingkoon@users.noreply.github.com>
2020-09-29 14:25:26 +0900

    tcp(2)
    tcp hw ver 2

--- ComputerNetwork/2-4/TCP2_4_cl.py
+++ ComputerNetwork/2-4/TCP2_4_cl.py
@@ -1,11 +1,25 @@
+#TCP
+#20151167 이인재
 from socket import *
+
+#서버이름, 포트번호 선언
 serverName = 'servername'
 serverPort = 12000
+
+#TCP소켓 생성
 clientSocket = socket(AF_INET, SOCK_STREAM)
+#서버이름과 서버 포트를 통한 소켓 연결
 clientSocket.connect((serverName, serverPort))
 
+#문자가 입력받을 수 있도록 input 함수 사용
 sentence = raw_input('input lowercase sentence:')
+#소켓을 통해 서버로 문장 전송
 clientSocket.send(sentence)
+#수정된 문장을 서버로부터 전송받음
 modifiedSentence = clientSocket.recv(1024)
+
+#수정된 문장을 출력
 print("From Server: ", modifiedSentence)
+
+#소켓 연결 종료
 clientSocket.close()
--- ComputerNetwork/2-4/TCP2_4_se.py
+++ ComputerNetwork/2-4/TCP2_4_se.py
@@ -1,14 +1,26 @@
+#TCP서버
+#20151167
 from socket import *
+
+#포트번호 설정
 serverPort = 12000
+
+#TCP소켓 생성
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
+
 print('The server is ready to receive')
 
 while True:
+    #서버소켓으로 연결
     connectionSocket, addr = serverSocket.accept()
 
+    #클라이언트로부터 입력 대기
     sentence = connectionSocket.recv(1024)
-    capitalizeSentence = sentence.upper()
+    #입력받은 문장을 대문자로 변경
+    capitalizedSentence = sentence.upper()
+    #대문자로 변경한 문장을 다시 클라이언트로 전달
     connectionSocket.send(capitalizedSentence)
+    #소켓 연결 종료
     connectionSocket.close()
