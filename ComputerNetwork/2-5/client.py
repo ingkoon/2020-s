b7816514b1b10cb16e58021e982313ba9e80383e
Orlando <59018852+ingkoon@users.noreply.github.com>
2020-10-07 02:52:38 +0900

    10/9 HW

--- ComputerNetwork/2-5/client.py
+++ ComputerNetwork/2-5/client.py
@@ -1,15 +1,25 @@
-from socket import *
-from pyowm import OWM
+#20151167 이인재
+#기상 알림프로그램 클라이언트
 
+#기상 상태 관리를 위한 pyowm, 통신을 위한 Socket 라이브러리 import
+from socket import *
+
+#연결을 위한 주소 및 포트번호 선언
 serverName = '127.0.0.1'
 serverPort = 12000
 
+#서버 포트번호 지정후 TCP 클라이언트 소켓 생성 및 연결
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
+#사용자로부터 지역 입력
 user_input = input('날씨를 알고싶은 지역을 입력하세요: ')
+#입력받은 데이터를 서버로 전달
 clientSocket.send(user_input.encode())
 
+#서버로 부터 날씨 데이터 전달받음
 w = clientSocket.recv(2048).decode()
+#입력받은 날씨 데이터를 출력
 print('%s\'s weather is %s' %(user_input, w))
+#소켓 통신 종료
 clientSocket.close()
--- ComputerNetwork/2-5/server.py
+++ ComputerNetwork/2-5/server.py
@@ -1,12 +1,18 @@
 #20151167 이인재
-
 #기상 알림프로그램 서버
+
+#기상 상태 관리를 위한 pyowm, 통신을 위한 Socket 라이브러리 import
 from socket import *
 from pyowm import OWM
 
+#API KEY를 받아 변수에 저장
 API_KEY = '1b394ff862cea8bb6c434072d2cd7c41'
+
+#API 관리 권한 임가
 owm = OWM(API_KEY)
-manager = owm.weather_manager()
+mgr = owm.weather_manager()
+
+#서버 포트번호 지정후 TCP서버 소켓 생성 및 바인딩
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('127.0.0.1',serverPort))
@@ -15,12 +21,15 @@ serverSocket.listen(1)
 print('server waiting...')
 
 while True:
+    #클라이언트로부터 입력 받아들임
     connection, addr = serverSocket.accept()
-
+    #입력 값을 변수에 할당
     loc = connection.recv(2048).decode()
-    obs = manager.weather_at_places(loc,'accurate')
-    w = obs.get_weather()
+    #해당 저장값을 기상 API에 인수로 전달
+    obs = mgr.weather_at_place(loc)
 
-    connction.send(w.get_status())
+    #날씨 데이터 변수에 할당
+    w = obs.weather
 
-    serverSocket.close()
+    #다시 클라이언트로 현재 해당 국가의 기상현황 전달
+    connection.send(w.status.encode())
