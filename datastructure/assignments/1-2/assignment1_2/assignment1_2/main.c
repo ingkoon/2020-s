b5167ab8f46ad822cb505b131d297c3ff9f0330b
Orlando <59018852+ingkoon@users.noreply.github.com>
2020-10-07 13:28:17 +0900

    huffman

---
+++ datastructure/assignments/1-2/assignment1_2/assignment1_2/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//huff파일을 읽어들여 freq파일로 출력하는 함수
+void rw_huff() {
+	int len;
+	char tfile[60];
+	int i;
+	char freq[30];
+
+}
+
+//freq파일을 읽어들여 huff-tree를 구성하는 함수
+void huff_tr() {
+}
+
+//트리를 이용하여 허프만 코드를 txt파일에 출력하는 함수
+void huff_code() {
+
+}
+
+//허프만 코드가 담긴 텍스트파일과 최초 메세지를 인코딩하는 함수
+void encoding(){
+}
+
+//인코딩된 메세지를 디코딩하여 원래 형태로 바꾸는 함수
+void decoding(){
+}
+
+//해당 과정을 출력하는 함수
+void seq_print() {
+}
+
+int main()
+{
+	rw_huff();
+
+	return 0;
+}
