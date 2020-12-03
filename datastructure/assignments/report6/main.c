a0efcd73bee976b814354db34c81ae26fd6dc87b
orlando <59018852+ingkoon@users.noreply.github.com>
2020-12-03 21:38:22 +0900

    Update main.c

--- datastructure/assignments/report6/main.c
+++ datastructure/assignments/report6/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INT_MAX 2147483647 // 최대 정수
-#define MAX_VERTICES 7  // 정점의 수
-#define INF 1000    // 무한대 (연결이 없는 경우)
+#define INT_MAX 2147483647
+// 최대 정수
+#define MAX 7
+// 정점의 수
+#define INF 1000
+// 연결이 없는 경우에 대한 무한 수
 
-int weight[MAX_VERTICES][MAX_VERTICES] = {
+//가중치 배열 초기화
+int weight[MAX][MAX] = {
   {0,7,INF,INF,3,10,INF},
   {7,0,4,10,2,6,INF},
   {INF,4,0,2,INF,INF,INF},
@@ -14,18 +18,23 @@ int weight[MAX_VERTICES][MAX_VERTICES] = {
   {10,6,INF,9,INF,0,INF},
   {INF,INF,INF,4,5,INF,0}
 };
-/* Dijkstra 변수*/
-int distance[MAX_VERTICES];
-int found[MAX_VERTICES];
-int route[MAX_VERTICES];
-int select(int distance[], int n, int found[]) {
+
+//최소거리 갱신을 위한 배열 선언
+int dst[MAX] = {0};
+//방문 여부를 체크하기 위한 배열 선언
+int found[MAX] = {0};
+//정점의 배열 선언
+int route[MAX] = {0};
+
+// 최소가중치를 통한 정점선택 함수
+int select(int n) {
     int min;
     int min_pos;
     min = INT_MAX;
     min_pos = -1;
     for (int i = 0; i < n; i++) {
-        if (distance[i] < min && found[i] == 0) {
-            min = distance[i];
+        if (dst[i] < min && found[i] == 0) {
+            min = dst[i];
             min_pos = i;
         }
     }
@@ -34,40 +43,50 @@ int select(int distance[], int n, int found[]) {
 
 void Shortest_Path(int start, int n) {
     // Dijkstra 알고리즘으로 최단 경로를 구한다.
-    // 시작 정점으로부터 그래프 내의 모든 정점에 대한 최단 경로를 구합니다.
+    // 그래프 내에 존재하는 모든 경로의 최단 경로를 구한다.
 
+    //그래프에 속한 각 정점에 대하여
     for (int i = 0; i < n; i++)
     {
-        distance[i] = weight[start][i]; // 시작 정점에서 다른 정점으로 향하는 가중치를 기준으로 초기화합니다.
+        // 최초의 가중치 초기화
+        dst[i] = weight[start][i];
         found[i] = 0;
     }
         found[start] = 1;
-    for (int i = 0; i < n - 1; i++) {
-        int u = select(distance, n, found);
-        // 최단 경로가 발견되지 않고, 시작 정점과의 거리가 가장 짧은 정점을 반환합니다.
-        found[u] = 1; // 해당 정점을 그래프에 포합합니다.
 
-        printf("%d is added to the Graph\n\n", u);
-        route[i+1] = u;
+    int cnt = 0;
+    printf("Start is 0\n");
+    //모든 정점이 포함되지 않을 때 까지
+    while (cnt != n-1) {
+        int u = select(n);
+        // 최단 경로가 발견되지 않고, 시작 정점과의 거리가 가장 짧은 정점을 반환
+        found[u] = 1; // 해당 정점을 그래프에 포합.
 
-        for (int j = 0; j < n; j++) {
-            if (found[j] == 0) {
-                if (distance[u] + weight[u][j] < distance[j]) {
-                    // 시작 정점에서 j로 바로 가는게 빠를지, u를 경유해서 가는게 빠를지 판단 합니다.
-                    printf("Before_distance update via[%d]: distance[%d] = %d\n", u, j, distance[j]);
-                    distance[j] = distance[u] + weight[u][j];
-                    printf("After_distance update via[%d]: distance[%d] = %d\n\n", u, j, distance[j]);
+        printf("%d add to array\n", u);
+        route[cnt+1] = u;
+
+        // v에 인접하고 s에 속하지 않는 정점에 대하여
+        for (int j = 0; j < n; j++)
+        {
+            if (found[j] == 0)
+            {
+                if (dst[u] + weight[u][j] < dst[j])
+                {
+                    dst[j] = dst[u] + weight[u][j];
                 }
             }
         }
+        cnt++;
     }
 }
 
 int main(void) {
-    Shortest_Path(0, MAX_VERTICES);
-    for(int i = 0; i< MAX_VERTICES; i++){
+    Shortest_Path(0, MAX);
+
+    for(int i = 0; i< MAX; i++){
       printf("%d ", route[i]);
     }
+    //정점을 순서대로 출력
       printf("\n");
 
       return 0;
